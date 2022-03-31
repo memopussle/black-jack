ebe6379ba89acb7e1db2bf4cb67c8249c12adb43
thugiang <xanhtham.cuc@gmail.com>
2022-03-31 22:32:49 +1300

    .

--- blacjack.js
+++ blacjack.js
@@ -74,15 +74,85 @@ function startGame() {
 
   //if dealerSum is < 17, we will give dealer a card
   while (dealerSum < 17) {
-    //<img>
-    let cardImg = document.createElement("img");
-    let card= deck.pop();
-    cardImg.src = "img/" + card + ".png";
-    dealerSum += getValue(card);
-    dealerAceCount += checkAce(card);
+    //<img src="img/A-C.png">
+    // we append the card into while loop until dealer has card with the sum of 17 or greater
+    let cardImg = document.createElement("img"); // create an imgcard
+    let card= deck.pop(); // got a card from deck
+    cardImg.src = "img/" + card + ".png"; // set the src of img card 
+    dealerSum += getValue(card);// increment dealerSum
+    dealerAceCount += checkAce(card); // count for the dealer's Ace
     document.getElementById("dealer-cards").append(cardImg);
   }
+  console.log(dealerSum);
+
+  //give user card
+  for (let i = 0; i < 2; i++) {
+    let cardImg = document.createElement("img"); // create an imgcard
+    let card = deck.pop(); // got a card from deck
+    cardImg.src = "img/" + card + ".png"; // set the src of img card
+    yourSum += getValue(card); // increment dealerSum
+    yourAceCount += checkAce(card); // count for the dealer's Ace
+    document.getElementById("your-cards").append(cardImg);
+  }
+  console.log(yourSum);
+
+  //implement hit button
+  document.getElementById("hit").addEventListener("click", hit);
+  //implement stand button
+   document.getElementById("stand").addEventListener("click", stand);
+
 }
+
+function hit() {
+  //if hit is true, give user a card
+  if (!canHit) {
+    return;
+  }
+   let cardImg = document.createElement("img");
+   let card = deck.pop(); 
+   cardImg.src = "img/" + card + ".png";
+   yourSum += getValue(card); 
+   yourAceCount += checkAce(card); 
+   document.getElementById("your-cards").append(cardImg);
+
+   //will check your Sum and take consideration into yourAcecount
+   if(reduceAce(yourSum, yourAceCount) > 21) { //A,J, 8 -> 1 + 10 + 8
+     canHit = false;
+   }
+}
+
+function stand() {
+  dealerSum = reduceAce(dealerSum, dealerAceCount);
+  yourSum = reduceAce(yourSum, yourAceCount);
+
+  canHit = false; // user is not able to draw card if choose stand
+  document.getElementById("hidden").src = "img/" + hidden + ".png";
+
+  let message ="";
+  if(yourSum > 21) {
+    message = "You loose";
+  } else if (dealerSum > 21) {
+    message = "You win!";
+  }
+  //both user and dealer have sum <=21
+  else if (yourSum == dealerSum) {
+    message ="Tie!";
+  }
+  else if ( yourSum > dealerSum) {
+    message = "You win!";
+  }
+  else if (yourSum < dealerSum) {
+    message = "You loose";
+  }
+
+  // populate the message onto #result
+  document.getElementById("result").innerText = message;
+  document.getElementById("dealer-sum").innerText = dealerSum;
+  document.getElementById("your-sum").innerText = yourSum;
+ 
+}
+
+
 
 function getValue(card) {
   let data = card.split("-"); // "4-C" -> splitting value into 2 parts; 4 & C -> [4,C]
@@ -106,4 +176,13 @@ function checkAce(card) {
     return 1;
   }
   return 0;
+}
+
+//reduce Ace if the sume is > 21
+function reduceAce(playerSum, playerAceCount) {
+  while(playerSum > 21 && playerAceCount > 0) {
+      playerSum -= 10;
+      playerAceCount -= 1;
+  }
+  return playerSum;
 }
